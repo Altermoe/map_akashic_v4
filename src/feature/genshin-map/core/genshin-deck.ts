992ef2e0ae4cffb6b0c8cf286a83754e9fa03d07
夕云葛城 <kuroneko7520@outlook.com>
2026-07-24 16:37:24 +0800

    refactor(genshin-deck): ♻️ 添加获取实时视图状态的方法

--- src/feature/genshin-map/core/genshin-deck.ts
+++ src/feature/genshin-map/core/genshin-deck.ts
@@ -1,5 +1,14 @@
 import { Deck } from 'deck.gl'
-import type { DeckProps, OrthographicView } from 'deck.gl'
+import type { DeckProps, OrthographicView, OrthographicViewState } from 'deck.gl'
 
-export class GenshinDeck extends Deck<OrthographicView> {}
+export class GenshinDeck extends Deck<OrthographicView> {
+  getLiveViewState(): OrthographicViewState | null {
+    const viewManager = this.viewManager
+    if (!viewManager) return null
+    const view = viewManager.views[0]
+    if (!view) return null
+    return viewManager.getViewState(view) as OrthographicViewState
+  }
+}
+
 export interface GenshinDeckProps extends DeckProps<OrthographicView> {}
