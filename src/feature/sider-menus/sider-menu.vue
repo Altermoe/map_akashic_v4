c5a4b434d29034ee9b36d605cf178afd8334e4ee
夕云葛城 <kuroneko7520@outlook.com>
2026-07-29 17:55:43 +0800

    feat(sider-menus): ✨ 新增侧边可拖拽面板组件

---
+++ src/feature/sider-menus/sider-menu.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { DraggablePanel } from '@/components/draggable-panel'
+</script>
+
+<template>
+  <DraggablePanel title="面板">
+    <p class="sider-menu__placeholder">
+      拖拽标题栏移动面板<br />
+      悬停边缘缩放面板
+    </p>
+  </DraggablePanel>
+</template>
+
+<style scoped>
+.sider-menu__placeholder {
+  margin: 0;
+  line-height: 1.6;
+  color: var(--gl-5);
+}
+</style>
