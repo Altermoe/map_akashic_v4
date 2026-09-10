c7c35820b4f90bda573c3f9b74fb77687b21896e
夕云葛城 <kuroneko7520@outlook.com>
2026-09-10 18:42:48 +0800

    refactor(index): ♻️ 注释重定向并新增页面跳转按钮

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-definePage({
-  redirect: '/map',
-})
+// definePage({
+//   redirect: '/map',
+// })
 </script>
 
 <template>
-  <div>index</div>
+  <div>
+    <router-link class="m-3 w-40 rounded block p-2 bg-gray-100" to="/map"> map </router-link>
+    <router-link class="m-3 w-40 rounded block p-2 bg-gray-100" to="/development/map-filter">
+      dev
+    </router-link>
+  </div>
 </template>
