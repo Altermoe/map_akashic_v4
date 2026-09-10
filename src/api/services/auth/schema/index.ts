c60a5605ede9a6b807b49ba088a07ed216bfbfaa
夕云葛城 <kuroneko7520@outlook.com>
2026-09-10 18:38:27 +0800

    docs(auth/schema): 📝 完善登录接口返回字段的注释说明

--- src/api/services/auth/schema/index.ts
+++ src/api/services/auth/schema/index.ts
@@ -2,9 +2,9 @@ import * as z from 'zod'
 
 export const loginResponseSchema = z.object({
   access_token: z.string().meta({ description: '接口 token' }),
-  token_type: z.string().meta({ description: 'token 前缀' }),
-  expires_in: z.int().meta({ description: '接口 token 有效时间' }),
+  token_type: z.string().meta({ description: 'token 前缀, 和 access_token 拼接后放在 header 中' }),
+  expires_in: z.int().meta({ description: '接口 token 有效时间, 单位为秒' }),
   scope: z.string().meta({ description: '可访问范围' }),
-  jti: z.string().meta({ description: "What's that mean?" }),
+  jti: z.string().meta({ description: 'JWT Identifier' }),
 })
 export type LoginResponse = z.infer<typeof loginResponseSchema>
